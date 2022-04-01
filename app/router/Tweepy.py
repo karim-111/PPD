49b9cf17e93b531ed230c6dc37272c42e689ea3d
KarimDex <karimoussaid07@gmail.com>
2022-04-01 13:32:26 +0200

    Add the Search by User_Name

--- app/router/Tweepy.py
+++ app/router/Tweepy.py
@@ -45,6 +45,24 @@ async def search(word, number, api):
         return df
 
 
+async def search_by_name(name, api):
+    posts = api.user_timeline(
+        screen_name=name, count=10, lang="en", tweet_mode="extended")
+    users_locs = [[
+        tweet.created_at,
+        tweet.full_text,
+        tweet.user.screen_name,
+        " ".join([hashtag['text'] for hashtag in tweet.entities['hashtags']]),
+        tweet.user.location,
+        tweet.source,
+        tweet.retweet_count
+    ] for tweet in posts]
+
+    df = pd.DataFrame(data=users_locs,
+                      columns=['Created_at', 'Text', 'user', 'hashtags', 'location', 'source', 'retweet_count'])
+    return df
+
+
 @router.get("/tweets/")
 async def info(settings: config.Settings = Depends(get_settings)):
 
@@ -53,6 +71,20 @@ async def info(settings: config.Settings = Depends(get_settings)):
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     df = await search("Bitcoin", 100, api)
+    print(df)
+    return {
+        "Succes": "Tweets are loaded"
+    }
+
+
+@router.get("/tweets_name/")
+async def info(settings: config.Settings = Depends(get_settings)):
+
+    auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+    auth.set_access_token(settings.ACCESS_KEY, settings.ACCESS_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    df = await search_by_name("musk", api)
     print(df)
     return {
         "Succes": "Tweets are loaded"
